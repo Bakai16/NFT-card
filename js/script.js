eb890f4aaf9872e7750b13c1b65f6dfdabe705b7
Bakai Akylbekov <bakayakylbekov3@gmail.com>
2022-02-13 20:13:13 +0600

    a

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
+window.addEventListener('load', function () {
+  const body = document.body;
+  const card = document.createElement("div");
+  const container = document.createElement("div");
+  const img = document.createElement("img");
+  const cardTitle = document.createElement("h3");
+  const cardDescr = document.createElement("p");
+  const cardCompany = document.createElement("ul");
+  const descr = document.createElement("li");
+  const images2 = document.createElement("img");
+  const cost = document.createElement("span");
+
+  const descr2 = document.createElement("li");
+  const img4 = document.createElement("img");
+  const lastDays = document.createElement("span");
+
+  const author = document.createElement("ul");
+  const authorImg = document.createElement("li");
+  const img3 = document.createElement("img");
+  const authorDesc = document.createElement("li");
+  const authorName = document.createElement("a");
 
 
-window.addEventListener("load", function () {
-    const body = document.body;
-    const card = document.createElement("div");
-    const container = document.createElement("div");
-    const img = document.createElement("img");
-    const cardTitle = document.createElement("h3");
-    const cardDescr = document.createElement("p");
-    const cardCompany = document.createElement("ul");
-    const descr = document.createElement("li");
-    const img2 = document.createElement("img");
-    const cost = document.createElement("span");
-    
-    const descr2 = document.createElement("li");
-    const img4 = document.createElement("img");
-    const lastDays = document.createElement("span");
-
-    const author = document.createElement("ul");
-    const authorImg = document.createElement("li");
-    const img3 = document.createElement("img");
-    const authorDesc = document.createElement("li");
-    const authorName = document.createElement("a");
-
-
-    body.append(card);
-    card.append(container);
-    container.append(img, cardTitle, cardDescr, cardCompany, author );
+  body.append(card);
+  card.append(container);
+  container.append(img, cardTitle, cardDescr, cardCompany, author);
 
   card.id = "wrapper";
 
@@ -41,7 +39,7 @@ window.addEventListener("load", function () {
         
   `;
 
-    card.style.cssText = `
+  card.style.cssText = `
     
     background-color: hsl(216, 50%, 16%);
     width: 300px;
@@ -52,7 +50,7 @@ window.addEventListener("load", function () {
 
 
 
-container.style.cssText = `
+  container.style.cssText = `
 margin: auto;
     
     width: 90%;
@@ -60,28 +58,28 @@ margin: auto;
 `;
 
 
-    
-img.setAttribute(
-      "src",
-      "./img/image-equilibrium.jpg"
-    );
-    img.style.cssText = `
+
+  img.setAttribute(
+    "src",
+    "./img/image-equilibrium.jpg"
+  );
+  img.style.cssText = `
     width: 100%;
     border-radius: 10px;
     margin: 15px auto;
   `;
-  
-    container.append(cardTitle);
-    cardTitle.textContent = "Equilibrium #3429";
-    cardTitle.style.cssText = `
+
+  container.append(cardTitle);
+  cardTitle.textContent = "Equilibrium #3429";
+  cardTitle.style.cssText = `
     color: white;
     font-weight: 600;
   `;
 
 
-    container.append(cardDescr);
-    cardDescr.textContent = "Our Equilibrium collection ptomotes balance and calm";
-    cardDescr.style.cssText = `
+  container.append(cardDescr);
+  cardDescr.textContent = "Our Equilibrium collection ptomotes balance and calm";
+  cardDescr.style.cssText = `
     color: hsl(215, 51%, 70%);
     font-weight: 400;
     
@@ -89,9 +87,9 @@ img.setAttribute(
 
 
 
-  
-    container.append(cardCompany);
-    cardCompany.style.cssText = `
+
+  container.append(cardCompany);
+  cardCompany.style.cssText = `
     display: flex;
     justify-content: space-between;
     font-weight: 400;
@@ -113,43 +111,43 @@ img.setAttribute(
   padding-bottom: 20px;
 `;
 
-descr.append(img2);
-img2.setAttribute(
+  descr.append(images2);
+  images2.setAttribute(
     "src",
     "./img/icon-ethereum.svg"
   );
 
 
-descr.append(cost);
-cost.textContent = ` 0.041ETH`;
+  descr.append(cost);
+  cost.textContent = ` 0.041ETH`;
 
 
 
-cardCompany.append(descr2);
-descr2.style.cssText = `
+  cardCompany.append(descr2);
+  descr2.style.cssText = `
   align-items: flex-start;
   font-weight: 400;
   //color: hsl(178, 100%, 50%);
   padding-bottom: 20px;
 `;
   descr2.append(img4);
-    img4.setAttribute(
+  img4.setAttribute(
     "src",
     "./img/icon-clock.svg"
   );
 
-  
+
   descr2.append(lastDays);
   lastDays.textContent = ` 3 day left`;
   lastDays.style.cssText = `
   font-weight: 400;
   color: hsl(215, 51%, 70%);
 `;
-  
 
-  
-    container.append(author);
-    author.style.cssText = `
+
+
+  container.append(author);
+  author.style.cssText = `
     display: flex;
     //justify-content: center;
     //vertical-align:center;
@@ -158,13 +156,13 @@ descr2.style.cssText = `
     padding: 0;
 `;
 
-    author.append(authorImg);
-    authorImg.append(img3);
-    img3.setAttribute(
-        "src",
-        "./img/image-avatar.png"
-      );
-      img3.style.cssText = `
+  author.append(authorImg);
+  authorImg.append(img3);
+  img3.setAttribute(
+    "src",
+    "./img/image-avatar.png"
+  );
+  img3.style.cssText = `
       width: 25px;
       border: 1px solid white;
       border-radius:20px;
@@ -173,9 +171,9 @@ descr2.style.cssText = `
       margin-right: 10px;
     `;
 
-    author.append(authorDesc);
-    authorDesc.textContent = ` Creation of `;
-    authorDesc.style.cssText = `
+  author.append(authorDesc);
+  authorDesc.textContent = ` Creation of `;
+  authorDesc.style.cssText = `
   
     font-weight: 400;
     color: white;
@@ -183,14 +181,14 @@ descr2.style.cssText = `
   
   `;
 
-    authorDesc.append(authorName);
-    authorName.textContent = ` Asan Asanov`;
-    authorName.style.cssText = `
+  authorDesc.append(authorName);
+  authorName.textContent = ` Asan Asanov`;
+  authorName.style.cssText = `
         
     font-weight: 400;
     color: hsl(178, 100%, 50%);
   
   `;
-  });
+});
 
-  alert(`Я оцениваю свою работу на ___ баллов`);
+alert(`Я оцениваю свою работу на ___ баллов`);
